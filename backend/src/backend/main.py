5212c6d972eb87d229e3e84761bf0d163286e703
bonnybabukachappilly <bonnybabukachappilly@gmail.com>
2026-07-05 12:13:46 +0530

    initial project setup

---
+++ backend/src/backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
